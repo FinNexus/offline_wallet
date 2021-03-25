5c8d908575012c3dbd0faa07d479de2741f70ac1
SmithAmbition <cranelvjq@163.com>
2021-03-25 20:49:30 +0800

    modify customChain set

--- transactions/EthTransaction.js
+++ transactions/EthTransaction.js
@@ -1,25 +1,6 @@
 let EthTx = require('ethereumjs-tx').Transaction;
 let IRawTransaction = require('./transaction.js').IRawTransaction;
 let Common = require('ethereumjs-common').default;
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-function forCustomChain(baseChain, customChainParams, hardfork, supportedHardforks) {
-    var standardChainParams = Common._getChainParams(baseChain);
-    return {
-        chainParams : __assign(__assign({}, standardChainParams), customChainParams),
-        hardfork : hardfork,
-        supportedHardforks : supportedHardforks
-    };
-};
 //normal transaction
 class EthTransaction extends IRawTransaction
 {
@@ -27,13 +8,14 @@ class EthTransaction extends IRawTransaction
     {
         super(trans);
         this.customChain = {
-            56 : forCustomChain('mainnet',
+            56 : {common: Common.forCustomChain('mainnet',
                 {
                 name: 'BSCTestNet',
                 networkId: 56,
                 chainId: 56,
-                }
-            )
+                },
+                'petersburg'
+            )}
         }
     }
     createTx(){
